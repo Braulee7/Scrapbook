0202dbddabde0d717529df997301482ce7ef796c
Braulee7 <braulio00001@gmail.com>
2023-09-14 07:21:49 -0700

    Added browser routing into application

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import "./index.css";
 import { getAuth } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Memory from "./pages/Memory";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   const [user] = useAuthState(getAuth());
-  return <>{user ? <Memory /> : <Login />}</>;
-  return <>{user ? <Home /> : <Login />}</>;
+  // return <>{user ? <Memory /> : <Login />}</>;
+  //  <>{user ? <Home /> : <Login />}</>
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      {user && <Route path="/" element={<Home />} />}
+      {user && <Route path="/memory/:memory" element={<Memory />} />}
+    </Routes>
+  );
 }
 
 export default App;
