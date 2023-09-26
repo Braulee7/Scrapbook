8e7d169ded91af917e3e533b8e7e98c731d991fe
Braulee7 <braulio00001@gmail.com>
2023-09-26 08:11:07 -0700

    Tuned the navigation buttons for the page TOC

--- src/components/page-toc/index.jsx
+++ src/components/page-toc/index.jsx
@@ -1,10 +1,14 @@
 import "./index.css";
 import { useState } from "react";
+import { SignOutUser } from "../../util/firebase";
 import AddPage from "../add-page";
 import Backdrop from "../backdrop";
 import GoHome from "../go-home";
+import ConfirmMessage from "../confirm-message";
+
 function PageTOC({ memory, pages, goToPage }) {
   const [adding, setAdding] = useState(false);
+  const [confirm, setConfirm] = useState(false);
   const toggleAddOn = (e) => {
     e.preventDefault();
     setAdding(true);
@@ -15,12 +19,27 @@ function PageTOC({ memory, pages, goToPage }) {
     setAdding(false);
   };
 
+  const handleClick = () => {
+    setConfirm(true);
+  };
+
+  const close = () => {
+    setConfirm(false);
+  };
+
   return (
     <>
       {adding && (
         <Backdrop callback={toggleAddOff}>
           <AddPage memory={memory} close={toggleAddOff} />
         </Backdrop>
+      )}
+      {confirm && (
+        <ConfirmMessage
+          message={"Sign out"}
+          confirmCallback={SignOutUser}
+          closeCallback={close}
+        />
       )}
       <div className="table-of-contents">
         <h1>{memory}</h1>
@@ -42,7 +61,12 @@ function PageTOC({ memory, pages, goToPage }) {
         <button onClick={toggleAddOn} className="toggle-add">
           Add new page
         </button>
-        <GoHome />
+        <div className="navigation-buttons">
+          <GoHome />
+          <button onClick={handleClick} className="sign-out">
+            Sign out
+          </button>
+        </div>
       </div>
     </>
   );
