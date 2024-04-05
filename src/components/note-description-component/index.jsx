75f5b79bbb62a7dc2f99e92f731c7bb96acaa146
Braulee7 <braulio00001@gmail.com>
2024-04-05 06:30:21 -0700

    fixed issue with rebase

--- src/components/note-description-component/index.jsx
+++ src/components/note-description-component/index.jsx
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
-=======
->>>>>>> 5cb10e1 (Made notecards draggable and removed description component)
 import NotecardContainer from "../notecard-container";
 import "./index.css";
 
 function NoteDescriptionComponent({ memory, page }) {
   return (
     <>
-<<<<<<< HEAD
-      {desc == null ? (
-        <Loading />
-      ) : (
-        <div className="note-desc-placeholder-container">
-          <div data-testid="note-description-div" className="note-desc-comp">
-            <NotecardContainer memory={memory} page={page} />
-          </div>
-=======
       <div className="note-desc-placeholder-container">
         <div data-testid="note-description-div" className="note-desc-comp">
           <NotecardContainer memory={memory} page={page} />
->>>>>>> 5cb10e1 (Made notecards draggable and removed description component)
         </div>
       </div>
     </>
