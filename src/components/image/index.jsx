cfa7f00e6da161bfb043d8866def9f04c099b75c
Braulee7 <braulio00001@gmail.com>
2024-04-09 10:59:28 -0700

    Implemented feature to edit images

--- src/components/image/index.jsx
+++ src/components/image/index.jsx
@@ -1,11 +1,19 @@
 import Draggable from "react-draggable";
 import { useRef, useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import { updateImagePosition } from "../../util/firebase";
+import SliderInput from "../slider-input";
+
 import "./index.css";
 
 function Image({ image, memory, page }) {
   const { nodeRef } = useRef(null);
   const [isMobile, setIsMobile] = useState(window.innerWidth < 1400);
+  const [rotate, setRotate] = useState(0);
+  const [scale, setScale] = useState(1);
+  const [hover, setHover] = useState(false);
+  const [edit, setEdit] = useState(false);
+
   useEffect(() => {
     const handleResize = () => {
       // if the page goes from row to column set
@@ -45,6 +53,19 @@ function Image({ image, memory, page }) {
     return pos;
   };
 
+  const variants = {
+    open: {
+      opacity: 1,
+      y: 0,
+      transition: { type: "spring", stiffness: 300, damping: 24 },
+    },
+    closed: {
+      opacity: 0,
+      y: 20,
+      transition: { duration: 0.2 },
+    },
+  };
+
   return (
     <>
       <Draggable
@@ -52,15 +73,48 @@ function Image({ image, memory, page }) {
         defaultPosition={getInitialPosition()}
         bounds=".page-container"
         onStop={handle_stop}
+        cancel=".img-edit-container"
       >
-        <div ref={nodeRef} className="single-image-container">
-          <img
+        <motion.div
+          ref={nodeRef}
+          className="single-image-container"
+          onHoverEnd={() => {
+            setHover(!hover);
+          }}
+          onHoverStart={() => {
+            setHover(!hover);
+          }}
+        >
+          <motion.button
+            onClick={() => {
+              setEdit(!edit);
+            }}
+            variants={variants}
+            animate={hover ? "open" : "closed"}
+          >
+            edit
+          </motion.button>
+
+          <motion.img
+            animate={{ rotate, scale }}
             draggable={false}
             className="firebase-image"
             src={image.url}
             alt={image.url}
           />
-        </div>
+          <motion.div
+            className="img-edit-container"
+            animate={edit ? "open" : "closed"}
+            variants={variants}
+          >
+            <SliderInput min={0.1} max={10} value={scale} set={setScale}>
+              Scale
+            </SliderInput>
+            <SliderInput min={-180} max={180} value={rotate} set={setRotate}>
+              Rotation
+            </SliderInput>
+          </motion.div>
+        </motion.div>
       </Draggable>
     </>
   );
---
+++ src/components/slider-input/index.jsx
@@ -0,0 +1,23 @@
+function SliderInput({ value, children, set, min = -200, max = 200 }) {
+  return (
+    <label>
+      <code>{children}</code>
+      <input
+        value={value}
+        type="range"
+        min={min}
+        max={max}
+        onChange={(e) => set(parseFloat(e.target.value))}
+      />
+      <input
+        type="number"
+        value={value}
+        min={min}
+        max={max}
+        onChange={(e) => set(parseFloat(e.target.value) || 0)}
+      />
+    </label>
+  );
+}
+
+export default SliderInput;
